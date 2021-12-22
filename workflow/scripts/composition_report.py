6388701e80062743c8be9387c8dfbda73f3203bc
Michael Hall <michael@mbh.sh>
2021-12-22 11:01:17 +1000

    more robust lineage and species handling

--- workflow/scripts/composition_report.py
+++ workflow/scripts/composition_report.py
@@ -1,13 +1,15 @@
 """This script generates a HTML file with a table containing information about the
 composition and lineage of each sample.
 """
+import re
 import sys
+from itertools import takewhile
 
 sys.stderr = open(snakemake.log[0], "w")
 import subprocess
 from collections import defaultdict
 from pathlib import Path
-from typing import TextIO, Tuple
+from typing import TextIO, Tuple, Optional, List, Union
 import json
 import jinja2
 import pandas as pd
@@ -59,6 +61,113 @@ def ripgrep_extract_covg(file: Path) -> float:
     return float(process.stdout.read().strip())
 
 
+LINEAGE_DELIM = "."
+LINEAGE_REGEX = re.compile(
+    rf"^(lineage)?(?P<major>[\w]+)\{LINEAGE_DELIM}?(?P<minor>.*)?$"
+)
+
+
+class InvalidLineageString(Exception):
+    pass
+
+
+class Lineage:
+    """A class representing a lineage and any of its sublineage information."""
+
+    def __init__(
+        self,
+        major: str,
+        minor: Optional[Union[str, List[str]]] = None,
+        minor_delim: str = ".",
+    ):
+        self.major = major
+        if minor is None:
+            minor = tuple()
+        if isinstance(minor, str):
+            minor = tuple(minor.split(minor_delim))
+        self.minor = tuple(minor)
+        self._minor_delim = minor_delim
+
+    def __str__(self):
+        if not self.minor:
+            return self.major
+        return self.major + self._minor_delim + self._minor_delim.join(self.minor)
+
+    @staticmethod
+    def from_str(s: str) -> "Lineage":
+        """Create a Lineage object from a str."""
+        match = LINEAGE_REGEX.search(s)
+        if not match:
+            return Lineage.from_name(s)
+        major = match.group("major")
+        minor = match.group("minor") or None
+        return Lineage(major=major, minor=minor)
+
+    @staticmethod
+    def from_name(s: str) -> "Lineage":
+        if "beijing" in s.lower():
+            lin = 2
+        elif "european" in s.lower():
+            lin = 4
+        elif "east_africa" in s.lower():
+            lin = 1
+        elif "central_asia" in s.lower():
+            lin = 3
+        else:
+            lin = s
+        return Lineage(lin)
+
+    def __eq__(self, other: "Lineage") -> bool:
+        return self.major == other.major and self.minor == other.minor
+
+    def __lt__(self, other: "Lineage") -> bool:
+        if (not self.minor and not other.minor) or not self.minor:
+            return False
+        if not other.minor:
+            return True
+        # we now know that both have minors
+        return len(self.minor) > len(other.minor)
+
+    def mrca(self, other: "Lineage") -> Optional["Lineage"]:
+        """Determine the most recent common ancestor between two Lineages.
+        Returns None if there is no MRCA.
+        """
+        if self.major != other.major:
+            return Lineage("Mixed")
+        if not self.minor or not other.minor:
+            return Lineage(major=self.major)
+
+        try:
+            common_minors, _ = zip(
+                *takewhile(lambda xy: xy[0] == xy[1], zip(self.minor, other.minor))
+            )
+            minor_str = LINEAGE_DELIM.join(common_minors)
+        except ValueError:
+            minor_str = None
+
+        return Lineage(self.major, minor_str)
+
+    @staticmethod
+    def call(lineages: List["Lineage"]) -> Optional["Lineage"]:
+        """Returns the Lineage with the most specific minor.
+        if the majors are different, returns None. If minors are the same, then takes
+        MRCA of the lineages with the same minor length.
+        """
+        if not lineages:
+            return Lineage("Unknown")
+        if len(lineages) == 1:
+            return lineages[0]
+        lineages.sort()
+        minors_of_same_len = filter(
+            lambda l: len(l.minor) == len(lineages[0].minor), lineages
+        )
+        lineage = lineages[0]
+        for lin in minors_of_same_len:
+            lineage = lin.mrca(lineage)
+
+        return lineage
+
+
 contam_warning = snakemake.params.get("contam_warning", 5.0) / 100
 unmapped_warning = snakemake.params.get("unmapped_warning", 5.0) / 100
 covg_warning = snakemake.params.get("covg_warning", 30)
@@ -90,6 +199,13 @@ def highlight_abnormal_lineages(col: pd.Series):
     return [f"background-color: {NORD12}" if v.isalpha() else "" for v in col]
 
 
+def highlight_abnormal_species(col: pd.Series):
+    """Highlights cells if their lineage is not one of the numbered majors."""
+    return [
+        f"background-color: {NORD12}" if "tuberculosis" not in v else "" for v in col
+    ]
+
+
 data = defaultdict(dict)
 logfiles = snakemake.input.filter_logs
 for file in map(Path, logfiles):
@@ -117,14 +233,19 @@ assignment_files = snakemake.input.lineages
 for file in map(Path, assignment_files):
     sample = file.name.split(".")[0]
     with open(file) as fp:
-        try:
-            lineage = json.load(fp)[sample]["phylogenetics"]["lineage"]["lineage"][0]
-        except KeyError as err:
-            raise KeyError(
-                f"Could not find key {err} for sample {sample} in file {file}"
-            )
-        lineage = lineage.replace("lineage", "")
-        data[sample]["lineage"] = lineage
+        phylo = json.load(fp)[sample]["phylogenetics"]
+        lineage_calls = phylo["lineage"]
+        if "lineage" in lineage_calls:
+            lineages = [Lineage.from_str(l) for l in lineage_calls["lineage"]]
+        else:
+            lineages = [Lineage.from_str(l) for l in lineage_calls.keys()]
+
+        data[sample]["lineage"] = Lineage.call(lineages)
+        species = list(phylo["species"].keys())
+        if len(species) != 1:
+            raise ValueError(f"Unexpected number of species {species}")
+        species = species[0]
+        data[sample]["species"] = species
 
 df = pd.DataFrame(data).T
 df.index.name = "sample"
@@ -133,6 +254,7 @@ percent_format_cols = [s for s in data[list(data.keys())[0]].keys() if s.endswit
 df_styled = (
     df.style.apply(highlight_high_contam, subset=["contam%"])
     .apply(highlight_abnormal_lineages, subset=["lineage"])
+    .apply(highlight_abnormal_species, subset=["species"])
     .apply(highlight_low_coverage, subset=["coverage"])
     .apply(highlight_high_unmapped, subset=["unmapped%"])
     .format("{:.2%}", subset=percent_format_cols)
